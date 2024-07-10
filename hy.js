79bdce6a4ec4edd271449f5955d5176eb562b5ce
Mo3M <86001815+Mo3M@users.noreply.github.com>
2024-07-10 22:19:42 +0330

    Update hy.js

--- hy.js
+++ hy.js
@@ -1,8 +1,10 @@
 function filter(node) {
   const info = JSON.parse(node.ProxyInfo);
-  if(node.Port.includes('8443'))     
+  if(node.Port.includes('8443')) {     
     return true;  
-  if(info.Port.includes('8443'))     
+  }
+  if(info.Port.includes('8443')) {    
     return true;  
+  }
   return false;
 }
